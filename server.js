17f448db66f7ab5a16f8b27da558aaeb5a639adf
Vinayak Nawdhar <vinayaknawdhar003@gmail.com>
2024-03-18 14:23:03 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 const server = createServer(app);
 const io = new Server(server,{
-  cors : {origin : ['http://localhost:3000','https://fio-food-in-and-out.vercel.app/']}
+  cors : {origin : ['http://localhost:3000','https://fio-food-in-and-out.vercel.app']}
 });
 
 app.get('/', (req, res) => {
